f6f49358ef4261049a9598687418143498604f54
Marjon Ramos <ramosmarjonf@gmail.com>
2020-03-26 07:55:34 +0800

    Crud for category

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $guarded = [];
 }
