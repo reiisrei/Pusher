73956c4b9b687be0e205fd6c2d07d512ed8f4bb9
Marjon Ramos <ramosmarjonf@gmail.com>
2020-03-25 23:47:49 +0800

    Initial Commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Model\Category;
+use App\Model\Question;
+use App\Model\Reply;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 10)->create();
+        factory(Category::class, 5)->create();
+        factory(Question::class, 10)->create();
+        factory(Reply::class, 50)->create()->each(function($reply){
+            return $reply->like()->save(factory(User::class)->make());
+        });
+    }
+}
