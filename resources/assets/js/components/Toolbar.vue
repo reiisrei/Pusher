609a06156e840247a6f42934f9c18369a0800374
Marjon Ramos <ramosmarjonf@gmail.com>
2020-04-09 23:44:48 +0800

    Vuetify, Vue Router and Initial components

---
+++ resources/assets/js/components/Toolbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+    tile
+  >
+    <v-toolbar dark>
+      <v-toolbar-title>Marjon</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>Forum</v-btn>
+            <v-btn flat>Ask Question</v-btn>
+            <v-btn flat>Category</v-btn>
+            <v-btn flat>Login</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
